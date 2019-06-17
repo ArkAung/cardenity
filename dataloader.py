e5dff9d6db19bece6e1722b41952ee8ecd96bb11
Arkar Aung <arica.aung@gmail.com>
2019-06-17 01:36:37 -0700

    Add model testing. Add more description for design choice. Add validation fraction parameter

--- dataloader.py
+++ dataloader.py
@@ -2,7 +2,7 @@ from fastai.vision import *
 
 
 def databunch_from_csv(csv_name, path, img_size, vertical_flip=False, batch_size=100,
-                       img_ext=None, train_folder=None):
+                       img_ext=None, train_folder=None, frac_validation=0.20):
     """Creates a databunch from csv label file"""
     data_transforms = get_transforms(do_flip=True, flip_vert=vertical_flip, max_rotate=5.0,
                                      max_zoom=1.1, max_lighting=0.2, max_warp=0.2, p_affine=0.2,
@@ -13,7 +13,7 @@ def databunch_from_csv(csv_name, path, img_size, vertical_flip=False, batch_size
                                        suffix=img_ext)
 
     data_src = (img_item_list
-                .split_by_rand_pct(valid_pct=0.15, seed=1337)
+                .split_by_rand_pct(valid_pct=frac_validation, seed=1337)
                 .label_from_df(label_delim=' '))
 
     databunch = (data_src
